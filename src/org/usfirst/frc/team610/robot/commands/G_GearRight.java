f65329b523d7cf40e502ddc3f2b100b20c362bd8
Patrick <pwblais@icloud.com>
2017-02-06 16:43:26 -0500

    Re-added G_GearRight

--- src/org/usfirst/frc/team610/robot/commands/G_GearRight.java
+++ src/org/usfirst/frc/team610/robot/commands/G_GearRight.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team610.robot.commands;
 
+import org.usfirst.frc.team610.robot.subsystems.DriveTrain;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
@@ -8,21 +10,11 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class G_GearRight extends CommandGroup {
 
     public G_GearRight() {
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
-
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
+    	
+    	DriveTrain.getInstance().resetEnc();
+    	addSequential(new A_PositionMove((13 * 12 + 9.5) * 12/17, 1));
+    	addSequential(new A_Turn(-30, 1));
+    	//Add forward until Optical Sensor reads the tape
+    	//Put gear on the peg
     }
 }
