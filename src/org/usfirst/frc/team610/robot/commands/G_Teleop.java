01e88c4c171d983ab703cb88064c6cc566c42bd2
charlesju <charlesju@crescentschool.org>
2017-02-08 00:15:12 -0500

    Cleaned Up Robot.java, deleted unused commands.
    Changed:
    - Removed A_Auton
    - Removed A_GearTurn
    - Removed G_PIDTesting
    - Renamed T_Teleop to G_Teleop

--- src/org/usfirst/frc/team610/robot/commands/G_Teleop.java
+++ src/org/usfirst/frc/team610/robot/commands/G_Teleop.java
@@ -2,8 +2,8 @@ package org.usfirst.frc.team610.robot.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
-public class T_Teleop extends CommandGroup{
-	public T_Teleop(){
+public class G_Teleop extends CommandGroup{
+	public G_Teleop(){
 		addParallel(new T_Drive());
 		addParallel(new T_GearIntake());
 //		addParallel(new T_Hanger());
