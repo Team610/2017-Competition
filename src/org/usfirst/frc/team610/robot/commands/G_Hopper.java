78babb6bbc755204aa5edc53243f1bd321b29056
maaranm <maaranmurugathas@crescentschool.org>
2017-02-09 07:48:25 -0500

    Added hopper auto

--- src/org/usfirst/frc/team610/robot/commands/G_Hopper.java
+++ src/org/usfirst/frc/team610/robot/commands/G_Hopper.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team610.robot.commands;
 
+import org.usfirst.frc.team610.robot.subsystems.DriveTrain;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
@@ -8,21 +10,12 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class G_Hopper extends CommandGroup {
 
     public G_Hopper() {
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
-
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
+    	DriveTrain.getInstance().resetEnc();
+    	addSequential(new A_PositionMove(132, 5));
+    	DriveTrain.getInstance().resetAngle();
+    	addSequential(new A_Turn(30,2));
+    	addSequential(new A_Turn(-120,2));
+    	DriveTrain.getInstance().resetEnc();
+    	addSequential(new A_PositionMove(84,5));
     }
 }
