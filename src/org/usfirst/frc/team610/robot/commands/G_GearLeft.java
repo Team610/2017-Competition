1eadacce4ad3074dd7d1fcc2d93a8614042695c9
BadgeSWS <jackson.brajer@gmail.com>
2017-02-10 16:17:52 -0500

    G_GearLeft

--- src/org/usfirst/frc/team610/robot/commands/G_GearLeft.java
+++ src/org/usfirst/frc/team610/robot/commands/G_GearLeft.java
@@ -13,9 +13,9 @@ public class G_GearLeft extends CommandGroup {
     public G_GearLeft() {
     	GearIntake.getInstance().setOuttake(false); //closes gear manip
     	GearIntake.getInstance().setIntake(true); //opens intake portion to let gear fall in & centers manip
-    	DriveTrain.getInstance().resetEnc();
-    	addSequential(new A_PositionMove(132, 1));
-    	addSequential(new A_TurnOptical(5,.5));
+    	DriveTrain.getInstance().resetEnc(); 
+    	addSequential(new A_PositionMove(76, 5));
+    	addSequential(new A_TurnOptical(5, -.2));
     	GearIntake.getInstance().setIntake(false); //lets intake float
     	addSequential(new A_PositionMove(36,5));
     	GearIntake.getInstance().setOuttake(true); //shoots gear
